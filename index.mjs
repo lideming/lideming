5cfca2477c4fa354c561d5a74f6a6f2118cbadcc
lideming <me@yuuza.net>
2021-05-19 02:11:17 +0800

    Add CI to update readme periodically

---
+++ index.mjs
@@ -0,0 +1,60 @@
+import mustache from 'mustache';
+import { $, cd } from 'zx';
+import { readFile, writeFile } from "fs/promises";
+import got from "got";
+
+await $`rm -rf dist`;
+await $`mkdir -p dist`;
+
+var readme = mustache.render(
+    await readFile('template.md', 'utf-8'),
+    {
+        listening: await getListening()
+    }
+);
+await writeFile('dist/README.md', readme, 'utf-8');
+
+await $`cp -r .git dist/`;
+
+cd('dist');
+
+await $`git branch -D main`;
+await $`git checkout --orphan main`;
+await $`git add .`;
+await $`git commit -m "Update from CI"`;
+await $`git push -f origin main`;
+
+
+async function getListening() {
+    var resp = await got('https://mc.yuuza.net/api/users/1/stat');
+    var json = JSON.parse(resp.body);
+    var track = json.lastPlayTrack;
+    var str = 'He ';
+    var timepass = Date.now() - new Date(json.lastPlayTime);
+    if (timepass < 1000 * 60 * 5)
+        str += 'is listening to ';
+    else
+        str += 'was listening to ';
+    if (track)
+        str += `[**${escapeMD(track.name)}** by **${escapeMD(track.artist)}**](https://mc.yuuza.net/#track/${track.id}) `;
+    else
+        str += 'unknown music ';
+    str += `(${formatTime(timepass)} ago).`;
+    return str;
+}
+
+function formatTime(ms) {
+    var unit = ['sec', 'min', 'hr', 'day'];
+    var step = [1000, 60, 60, 24];
+    var val = ms / step[0];
+    var cur = 1;
+    while (cur < unit.length && val >= step[cur]) {
+        val /= step[cur];
+        cur++;
+    }
+    return val.toFixed(0) + ' ' + unit[cur - 1];
+}
+
+function escapeMD(str) {
+    return str.replace(/[\[\]*_<>&]/g, x => '\\' + x);
+}
